286bec17596a2041ce6553c78752e19e3b388a9a
Naz <nchinazaeze@gmail.com>
2017-05-01 23:08:08 +0100

    Added solution for single letter transcription

--- java/rna-transcription/src/main/java/RnaTranscription.java
+++ java/rna-transcription/src/main/java/RnaTranscription.java
@@ -1,8 +1,21 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class RnaTranscription {
 
+    private static final Map<String, String> TRANSCRIPTION;
+
+    static {
+        TRANSCRIPTION = new HashMap<>();
+        TRANSCRIPTION.put("C", "G");
+        TRANSCRIPTION.put("G", "C");
+        TRANSCRIPTION.put("T", "A");
+        TRANSCRIPTION.put("A", "U");
+
+    }
+
     public String ofDna(String dnaString) {
-        String result = "";
-        return result;
+        return dnaString.equals("") ? "" : TRANSCRIPTION.get(dnaString);
     }
 
 }
--- java/rna-transcription/src/test/java/RnaTranscriptionTest.java
+++ java/rna-transcription/src/test/java/RnaTranscriptionTest.java
@@ -17,25 +17,21 @@ public class RnaTranscriptionTest {
         Assert.assertEquals("", rnaTranscription.ofDna(""));
     }
 
-    @Ignore("Remove to run test")
     @Test
     public void testRnaTranscriptionOfCytosineIsGuanine() {
         Assert.assertEquals("G", rnaTranscription.ofDna("C"));
     }
 
-    @Ignore("Remove to run test")
     @Test
     public void testRnaTranscriptionOfGuanineIsCytosine() {
         Assert.assertEquals("C", rnaTranscription.ofDna("G"));
     }
 
-    @Ignore("Remove to run test")
     @Test
     public void testRnaTranscriptionOfThymineIsAdenine() {
         Assert.assertEquals("A", rnaTranscription.ofDna("T"));
     }
 
-    @Ignore("Remove to run test")
     @Test
     public void testRnaTranscriptionOfAdenineIsUracil() {
         Assert.assertEquals("U", rnaTranscription.ofDna("A"));
