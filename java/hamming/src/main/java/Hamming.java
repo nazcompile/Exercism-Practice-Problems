0243650c0dce0d41fafa823c29b82e866458b2f6
Naz <nchinazaeze@gmail.com>
2017-05-13 12:04:38 +0100

    Added solution for Hamming exercise

--- java/hamming/src/main/java/Hamming.java
+++ java/hamming/src/main/java/Hamming.java
@@ -1,2 +1,25 @@
+import java.util.stream.IntStream;
+
 public class Hamming {
+
+    private String firstStrand;
+    private String secondStrand;
+
+    public Hamming(String firstStrand, String secondStrand) {
+        this.firstStrand = firstStrand;
+        this.secondStrand = secondStrand;
+        validateInput();
+    }
+
+    public int getHammingDistance() {
+        return Math.toIntExact(
+                IntStream.range(0, firstStrand.length())
+                        .filter(index -> firstStrand.charAt(index) != secondStrand.charAt(index))
+                        .count());
+    }
+
+    private void validateInput() {
+        if (firstStrand.length() != secondStrand.length()) throw new IllegalArgumentException();
+    }
+
 }
