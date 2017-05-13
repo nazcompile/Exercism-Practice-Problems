6e4f621cfbad4230aa098a1cbf5e21fad1788fd9
Naz <nchinazaeze@gmail.com>
2017-05-13 12:29:30 +0100

    Extracted total number of alphabets to a constant field

--- java/pangram/src/main/java/PangramChecker.java
+++ java/pangram/src/main/java/PangramChecker.java
@@ -1,11 +1,13 @@
 public class PangramChecker {
 
+    static final int TOTAL_NUM_OF_ALPHABETS = 26;
+
     public boolean isPangram(String input) {
         boolean result = false;
         if (input != "") {
             result = input.toUpperCase().chars()
                           .filter(chars -> chars >= 'A' && chars <= 'Z')
-                          .distinct().count() == 26;
+                          .distinct().count() == TOTAL_NUM_OF_ALPHABETS;
         }
         return result;
     }
