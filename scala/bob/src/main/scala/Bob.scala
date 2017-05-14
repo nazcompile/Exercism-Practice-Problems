aa78a91ab26186e5f91f24a76efb3fe8206cac6e
Naz <nchinazaeze@gmail.com>
2017-05-14 13:53:16 +0100

    Added Solution for Bob exercise

--- scala/bob/src/main/scala/Bob.scala
+++ scala/bob/src/main/scala/Bob.scala
@@ -1,9 +1,13 @@
 
 class Bob {
 
-  def hey(statement: String) = {
-    var answer = "Whatever."
-    answer
+
+  def hey(statement: String): String = {
+    if (statement.matches(".*[A-Z].*") &&
+        statement == statement.toUpperCase()) return "Whoa, chill out!"
+    else if (statement.endsWith("?")) return "Sure."
+    else if (statement.trim.isEmpty) return "Fine. Be that way!"
+    else return "Whatever."
   }
 
 }
