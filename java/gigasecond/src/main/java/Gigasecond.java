c80a800ede51d6dc9be5c5f8baead188fc6aa316
Naz <nchinazaeze@gmail.com>
2017-05-16 22:53:16 +0100

    Added solution for Gigasecond exercise

--- java/gigasecond/src/main/java/Gigasecond.java
+++ java/gigasecond/src/main/java/Gigasecond.java
@@ -1,12 +1,20 @@
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.temporal.TemporalUnit;
+
 public class Gigasecond<T> {
 
-    private T date;
+    private LocalDateTime date;
 
-    public Gigasecond(T date) {
+    public Gigasecond(LocalDate date) {
+        this.date = date.atTime(0, 0);
+    }
+
+    public Gigasecond(LocalDateTime date) {
         this.date = date;
     }
 
-    public T getDate() {
-        return null;
+    public LocalDateTime getDate() {
+        return date.plusSeconds(100_000_0000);
     }
 }
