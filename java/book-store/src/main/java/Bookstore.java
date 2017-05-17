02182f2108932ca5e248a2f4db8bea34d5d784d6
Naz <nchinazaeze@gmail.com>
2017-05-17 21:27:26 +0100

    Initial commit for book-store exercise

---
+++ java/book-store/src/main/java/Bookstore.java
@@ -0,0 +1,15 @@
+import java.util.List;
+
+class Bookstore {
+
+    private List<Integer> books;
+
+    Bookstore(List<Integer> books) {
+        this.books = books;
+    }
+
+    double calculateTotalCost() {
+        return 0;
+    }
+
+}
