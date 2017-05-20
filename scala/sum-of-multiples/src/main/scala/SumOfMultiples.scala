e39e583253b7abc53256f9c5caa67c061cfb3182
Naz <nchinazaeze@gmail.com>
2017-05-20 20:46:23 +0100

    Added solution for SumOfMultiples exercise

--- scala/sum-of-multiples/src/main/scala/SumOfMultiples.scala
+++ scala/sum-of-multiples/src/main/scala/SumOfMultiples.scala
@@ -1,4 +1,11 @@
 object SumOfMultiples {
-  def sumOfMultiples(factors: Set[Int], limit: Int): Int = ???
+
+  def sumOfMultiples(factors: Set[Int], limit: Int): Int = {
+
+    1.until(limit)
+      .filter ((index: Int) => factors.exists(value => index % value == 0))
+      .sum
+  }
+
 }
 
